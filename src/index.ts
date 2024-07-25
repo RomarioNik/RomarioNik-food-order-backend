ecb588029ec865a88976715e2e9ffe37bbd33a37
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2024-07-25 23:34:07 +0100

    connect database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+const { MONGO_CONNECTION, PORT_SERVER } = process.env;
+
+mongoose
+  .connect(MONGO_CONNECTION as string)
+  .then(() => console.log("Connected to database!"));
 
 const app = express();
 
@@ -11,7 +18,7 @@ app.get("/test", (req: Request, res: Response) => {
   res.json({ message: "Hello!" });
 });
 
-app.listen(7000, () => {
+app.listen(PORT_SERVER, () => {
   console.log("server started on port localhost:7000");
 });
 
